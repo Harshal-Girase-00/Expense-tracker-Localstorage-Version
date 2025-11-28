c2eed6ca612d9e40918816381be24be78eaa5432
Harshal Rajput <giraseharshal.302@gmail.com>
2025-11-28 12:12:08 +0530

    add icons

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: "autoUpdate",
-      includeAssets: ["favicon.ico", "apple-touch-icon.webp", "mask-icon.svg"],
+      includeAssets: ["favicon.ico", "apple-touch-icon.png"],
       manifest: {
         name: "Expense Tracker",
         short_name: "ExpTracker",
@@ -17,14 +17,14 @@ export default defineConfig({
         display: "standalone",
         icons: [
           {
-            src: "pwa-192x192.webp",
+            src: "pwa-192-192.webp",
             sizes: "192x192",
-            type: "image/png",
+            type: "image/webp",
           },
           {
-            src: "pwa-512x512.webp",
+            src: "pwa-512-512.webp",
             sizes: "512x512",
-            type: "image/png",
+            type: "image/webp",
           },
         ],
       },
